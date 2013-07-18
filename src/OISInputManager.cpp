5844b5fa2763f20a2771c8efca99806cd5307a94
Christopher S. Case <chris.case@g33xnexus.com>
2013-07-18 09:30:55 -0500

    added the ability for OIS to handle getting passed an NSView, instead of just an NSWindow, since Qt on OSX only gives us the NSView.

--- src/OISInputManager.cpp
+++ src/OISInputManager.cpp
@@ -6,16 +6,16 @@ Copyright (c) 2005-2007 Phillip Castaneda (pjcast -- www.wreckedgames.com)
 This software is provided 'as-is', without any express or implied warranty. In no event will
 the authors be held liable for any damages arising from the use of this software.
 
-Permission is granted to anyone to use this software for any purpose, including commercial 
+Permission is granted to anyone to use this software for any purpose, including commercial
 applications, and to alter it and redistribute it freely, subject to the following
 restrictions:
 
-    1. The origin of this software must not be misrepresented; you must not claim that 
-		you wrote the original software. If you use this software in a product, 
-		an acknowledgment in the product documentation would be appreciated but is 
+    1. The origin of this software must not be misrepresented; you must not claim that
+		you wrote the original software. If you use this software in a product,
+		an acknowledgment in the product documentation would be appreciated but is
 		not required.
 
-    2. Altered source versions must be plainly marked as such, and must not be 
+    2. Altered source versions must be plainly marked as such, and must not be
 		misrepresented as being the original software.
 
     3. This notice may not be removed or altered from any source distribution.
@@ -121,6 +121,8 @@ InputManager* InputManager::createInputSystem( ParamList &paramList )
     {
 #ifdef __OBJC__
         id obj = (id)strtoul(i->second.c_str(), 0, 10);
+        if(obj && [obj isKindOfClass:[NSView class]])
+			obj = [obj window];
         if(obj && [obj isKindOfClass:[NSWindow class]])
             im = new CocoaInputManager();
 #endif
@@ -133,7 +135,7 @@ InputManager* InputManager::createInputSystem( ParamList &paramList )
 	im = new iPhoneInputManager();
 #else
 	OIS_EXCEPT(E_General, "No platform library.. check build platform defines!");
-#endif 
+#endif
 
 	try
 	{
@@ -155,7 +157,7 @@ void InputManager::destroyInputSystem(InputManager* manager)
 		return;
 
 	//Cleanup before deleting...
-	for( FactoryCreatedObject::iterator i = manager->mFactoryObjects.begin(); 
+	for( FactoryCreatedObject::iterator i = manager->mFactoryObjects.begin();
 		i != manager->mFactoryObjects.end(); ++i )
 	{
 		i->second->destroyObject( i->first );
